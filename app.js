6906c54a1e1d0db3c73ebedf00b9e3031f086198
khoi148 <khoiminhphan@yahoo.com>
2020-04-13 18:33:29 +0700

    added in all CLI commands, done

--- app.js
+++ app.js
@@ -15,17 +15,34 @@ function loadData() {
   return JSON.parse(data); //convert json string to JS object
 }
 function saveData(data) {
-  let curr = loadData();
-  curr.push(data);
-  fs.writeFileSync("data/database.json", JSON.stringify(curr));
+  fs.writeFileSync("data/database.json", JSON.stringify(data));
 }
 function addTodo(todoBody, status) {
   let curr = loadData();
   if (curr.length !== 0) {
-    currId = ++curr[Math.max(curr.length - 1, 0)].id;
+    currId = curr[curr.length - 1].id;
+    currId++;
   }
-  console.log({ id: currId, todo: todoBody, status: status });
-  saveData({ id: currId, todo: todoBody, status: status });
+  let newTodo = { id: currId, todo: todoBody, status: status };
+  displayItem(newTodo);
+  curr.push(newTodo);
+  saveData(curr);
+}
+function display(data) {
+  //console.log(data);
+  if (data.length === 0) {
+    console.log(chalk.blue.bold("No items to show"));
+  } else {
+    data.forEach((item) => {
+      let str = `id: ${item.id}  todo: ${item.todo}  status: ${item.status}`;
+      if (item.status === true) console.log(chalk.green.bold(str));
+      else console.log(chalk.red.bold(str));
+    });
+  }
+}
+function displayItem(data) {
+  let str = `Created a todo: "${data.todo}"`;
+  console.log(chalk.blue.bold(str));
 }
 
 /* //How to process CLI args manually
@@ -40,6 +57,63 @@ if (process.argv[2] === "add") {
 
 //How to use a library called yargs !!! for handling CLI input/output
 //add 2 yarg commands
+
+yargs.command({
+  command: "list-complete",
+  describe: "list contents of json",
+  builder: {},
+  handler: function () {
+    display(
+      loadData().filter((item) => {
+        return item.status === true;
+      })
+    );
+  },
+});
+yargs.command({
+  command: "list-incomplete",
+  describe: "list contents of json",
+  builder: {},
+  handler: function () {
+    display(
+      loadData().filter((item) => {
+        return item.status === false;
+      })
+    );
+  },
+});
+
+const COMMANDS = [
+  "list",
+  "list-commands",
+  "list-complete",
+  "list-incomplete",
+  "add --todo=<content here>",
+  "delete <id>",
+  "delete-all",
+  "delete-completed",
+  "toggle <id>",
+];
+yargs.command({
+  command: "list-commands",
+  describe: "list cli commands available",
+  builder: {},
+  handler: function () {
+    //let arrayOfCommands = COMMANDS;
+    COMMANDS.forEach((str) => {
+      console.log(chalk.yellow(str));
+    });
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "list contents of json",
+  builder: {},
+  handler: function () {
+    display(loadData());
+  },
+});
+
 yargs.command({
   command: "add",
   describe: "add some todo",
@@ -51,7 +125,7 @@ yargs.command({
       default: "nothing",
     },
     status: {
-      describe: "status of your todo",
+      describe: "status of your todo. True means it's done",
       demandOption: false,
       default: false,
       type: "boolean",
@@ -62,12 +136,61 @@ yargs.command({
     addTodo(todo, status);
   },
 });
+//the command will always be on process.argv[2]. argv[3...] will be the IDs
+//The 1st/2nd index is two path directories to node and the current project
 yargs.command({
-  command: "list",
-  describe: "list contents of json",
+  command: "delete",
+  describe: "delete an todo items in your json, based on id",
   builder: {},
   handler: function () {
-    console.log(loadData());
+    let args = [...process.argv];
+    let data = loadData();
+    if (args.length > 3) {
+      for (let x = 3; x < args.length; x++) {
+        let currId = parseInt(process.argv[x]);
+        data = data.filter((item) => {
+          return item.id !== currId;
+        });
+      }
+    }
+    // let id = parseInt(process.argv[3]);
+    saveData(data);
   },
 });
+yargs.command({
+  command: "delete-all",
+  describe: "delete all items in your json",
+  builder: {},
+  handler: function () {
+    saveData([]);
+  },
+});
+yargs.command({
+  command: "delete-completed",
+  describe: "delete items in your json, that are completed",
+  builder: {},
+  handler: function () {
+    let result = loadData().filter((item) => {
+      return item.status === false;
+    });
+    saveData(result);
+  },
+});
+yargs.command({
+  command: "toggle",
+  describe: "switch the status of a todo item",
+  builder: {},
+  handler: function () {
+    let selectedId = parseInt(process.argv[3]);
+    let result = loadData().map((item) => {
+      if (item.id === selectedId) {
+        item.status = !item.status;
+        return item;
+      }
+      return item;
+    });
+    saveData(result);
+  },
+});
+
 yargs.parse(); //how to call yargs
